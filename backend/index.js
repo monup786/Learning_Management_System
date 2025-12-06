7a904a11d1b2ceb66ec56d4dd510c1a191759be1
monup786 <monupandit79988@gmail.com>
2025-12-07 00:38:18 +0530

    Update CORS origin to include multiple domains

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,10 @@ const port=process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:"https://learning-management-system-1-0vos.onrender.com",
+    origin:["https://learning-management-system-1-0vos.onrender.com",
+           "https://learning-management-system-puvm.onrender.com",
+            "http://localhost:5173"
+           ],
     credentials:true
 }));
 
